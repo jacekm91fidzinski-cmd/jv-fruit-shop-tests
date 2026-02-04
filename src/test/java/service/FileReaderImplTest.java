2aa0a778582b748041cd1ccc03086115587e442d
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2026-02-04 15:45:11 +0100

    jv-fruit-shop-tests

--- src/test/java/service/FileReaderImplTest.java
+++ src/test/java/service/FileReaderImplTest.java
@@ -1,7 +1,6 @@
 package service;
 
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/service/ReportGeneratorImplTest.java
+++ src/test/java/service/ReportGeneratorImplTest.java
@@ -2,6 +2,7 @@ package service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ReportGeneratorImplTest {
